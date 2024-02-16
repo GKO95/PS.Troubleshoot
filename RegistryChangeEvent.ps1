479c8fe7e684d70e2f489a852128832ea26247ca
Gihawn Ko <44629526+GKO95@users.noreply.github.com>
2024-02-16 23:09:11 +0900

    Update RegistryChangeEvent.ps1

--- RegistryChangeEvent.ps1
+++ RegistryChangeEvent.ps1
@@ -1,14 +1,31 @@
+# RegistryEvent: https://learn.microsoft.com/en-us/previous-versions/windows/desktop/regprov/registryevent
 $regevt = "RegistryKeyChangeEvent"
-$reg = @{ Hive = "HKEY_LOCAL_MACHINE"; Path = "SYSTEM\\CurrentControlSet\\Control\\CrashControl" }
+$reg = @{ 
+    Hive = "HKEY_LOCAL_MACHINE"; 
+    Path = "SYSTEM\\CurrentControlSet\\Control\\CrashControl";
+    Value = "";
+}
 
-# Create a ManagementEventWatcher object
+# Check supported registry hives
+switch -Exact ($Hive) {
+    "HKEY_USERS" { Break }
+    "HKEY_LOCAL_MACHINE" { Break }
+    "HKEY_CURRENT_CONFIG" { Break }
+    "HKEY_CLASSES_ROOT" { Write-Error "Unsupported registry hive."; Exit }
+    "HKEY_CURRENT_USER" { Write-Error "Unsupported registry hive."; Exit }
+    Default { Write-Error "Invalid registry hive."; Exit }
+}
+
+# Generate a WMI query based on the registry event type
 $query = "SELECT * FROM $($regevt) WHERE Hive='$($reg['Hive'])'"
 switch -Exact ($regevt) {
-    "RegistryTreeChangeEvent" { $query += " AND RootPath='$($reg['Path'])'"; Break }
-    "RegistryKeyChangeEvent" { $query += " AND KeyPath='$($reg['Path'])'"; Break }
-    "RegistryValueChangeEvent" { $query += " AND KeyPath='$($reg['Path'])' AND ValueName='$($reg['Value'])'"; Break }
+    "RegistryTreeChangeEvent" { $query += " AND RootPath='$($reg['Path'])'" }
+    "RegistryKeyChangeEvent" { $query += " AND KeyPath='$($reg['Path'])'" }
+    "RegistryValueChangeEvent" { $query += " AND KeyPath='$($reg['Path'])' AND ValueName='$($reg['Value'])'" }
     Default { Write-Error "Invalid registry event."; Exit }
 }
+
+# Create a ManagementEventWatcher object
 $watcher = New-Object System.Management.ManagementEventWatcher $query
 
 # Register an event that gets fired when an event arrives
@@ -16,6 +33,11 @@ Register-ObjectEvent -InputObject $watcher -EventName EventArrived -Action {
     $newEvent = $event.SourceEventArgs.NewEvent
     Write-Host "Event occurred!"
     Write-Host ($newEvent | Format-List | Out-String)
+
+        # Write your code here
+
+    # Stop the watcher
+    $watcher.Stop()
 } | Out-Null
 
 Write-Host ($reg | Format-Table | Out-String)
